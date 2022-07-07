39d6b61fdc3cc756102c3d924d728edc92e84c0c
PukiTaiLin <108761895+PukiTaiLin@users.noreply.github.com>
2022-07-07 11:47:20 +0300

    Update AzDeployScript.ps1

--- AzDeployScript.ps1
+++ AzDeployScript.ps1
@@ -1,3 +1,3 @@
-New-AzResourceGroup -Name myResourceGroupInbal02 -Location "Central US"
-New-AzResourceGroupDeployment -Name TwoStorateAccounts01 -ResourceGroupName myResourceGroupInbal02 -TemplateFile '.\template1 - 1.2.json'
-New-AzResourceGroupDeployment -Name NewVMTest00 -ResourceGroupName myResourceGroupInbal02 -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
+New-AzResourceGroup -Name myResourceGroupInbal06 -Location "Central US"
+New-AzResourceGroupDeployment -Name TwoStorateAccounts01 -ResourceGroupName myResourceGroupInbal06 -TemplateFile '.\template1 - 1.2.json'
+New-AzResourceGroupDeployment -Name NewVMTest00 -ResourceGroupName myResourceGroupInbal06 -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
