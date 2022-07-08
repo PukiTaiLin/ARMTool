5407fc0137a2d82d53ef744db2a11b6d73c6ec05
PukiTaiLin <108761895+PukiTaiLin@users.noreply.github.com>
2022-07-08 18:33:20 +0300

    Add files via upload

---
+++ BlobsScript.ps1
@@ -0,0 +1,31 @@
+
+#Create containerA with context to the storage accounts(Container permmision? -Permission Blob)
+$ctx1 = New-AzStorageContext -StorageAccountName inbstorageaccount1 -UseConnectedAccount
+New-AzStorageContainer -Name inbalcontainera -Context $ctx1 
+#Create containerB with context to the storage accounts(Container permmision? -Permission Blob)
+$ctx2 = New-AzStorageContext -StorageAccountName inbstorageaccount0 -UseConnectedAccount
+New-AzStorageContainer -Name inbalcontainerb -Context $ctx2
+
+#Create 100 blobs
+function Create100BlobsInNewFolder 
+{
+    Write-Output "                              start blob"
+    #ToDo: create folder
+    New-Item -ItemType Directory -Force -Path 100_Blobs
+    for( $num = 0; $num -le 1  ; $num++ )
+    {
+        Write-Output $num
+        $fileToCreate = ".\100_Blobs\$File$num.txt"
+        Set-Content $fileToCreate 'Blob'
+        #Upload the New Blob into ContainerA 
+        Set-AzStorageBlobContent -Container inbalcontainera -File $File$num -Context 
+    }
+    Write-Output "finish blob"
+}
+Create100BlobsInNewFolder
+
+
+#copy from A to B
+#need a loop to copy each one of the blobs
+#Copy a blob synchronously.     
+#Copy-AzStorageBlob -SrcContainer $SrcContainer -DestContainer $DestContainer
