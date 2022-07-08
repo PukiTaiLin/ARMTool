36c60f3fcf9f614a61b91218a61c4c5c2364936c
PukiTaiLin <108761895+PukiTaiLin@users.noreply.github.com>
2022-07-08 21:05:05 +0300

    Add files via upload

--- AzDeployScript.ps1
+++ AzDeployScript.ps1
@@ -1,3 +1,3 @@
-New-AzResourceGroup -Name myResourceGroupInbal06 -Location "Central US"
-New-AzResourceGroupDeployment -Name TwoStorateAccounts01 -ResourceGroupName myResourceGroupInbal06 -TemplateFile '.\template1 - 1.2.json'
-New-AzResourceGroupDeployment -Name NewVMTest00 -ResourceGroupName myResourceGroupInbal06 -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
+New-AzResourceGroup -Name myResourceGroupInbal -Location "Central US"
+New-AzResourceGroupDeployment -Name createTwoStorateAccounts -ResourceGroupName myResourceGroupInbal -TemplateFile '.\template1 - 1.2.json'
+New-AzResourceGroupDeployment -Name NewVM -ResourceGroupName myResourceGroupInbal -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
