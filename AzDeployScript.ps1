f330b95fa392a18ab85c379396d6da7ea42b1b26
PukiTaiLin <108761895+PukiTaiLin@users.noreply.github.com>
2022-07-08 21:54:33 +0300

    Update AzDeployScript.ps1

--- AzDeployScript.ps1
+++ AzDeployScript.ps1
@@ -1,3 +1,3 @@
 New-AzResourceGroup -Name myResourceGroupInbal -Location "Central US"
-New-AzResourceGroupDeployment -Name createTwoStorateAccounts -ResourceGroupName myResourceGroupInbal -TemplateFile '.\template1 - 1.2.json'
+New-AzResourceGroupDeployment -Name createtwotorate -ResourceGroupName myResourceGroupInbal -TemplateFile '.\template1 - 1.2.json'
 New-AzResourceGroupDeployment -Name NewVM -ResourceGroupName myResourceGroupInbal -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
