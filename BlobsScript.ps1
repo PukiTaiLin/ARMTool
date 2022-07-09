f2c45abdbfd0d909f566bbabef9c570e0d946607
PukiTaiLin <108761895+PukiTaiLin@users.noreply.github.com>
2022-07-09 23:22:11 +0300

    Add files via upload

---
+++ BlobsScript.ps1
@@ -0,0 +1,42 @@
+#Create containerA 
+$ctx1 = New-AzStorageContext -StorageAccountName inbstorageaccount1 -UseConnectedAccount
+New-AzStorageContainer -Name containertesta -Context $ctx1 -Permission Blob
+#Create containerB 
+$ctx2 = New-AzStorageContext -StorageAccountName inbstorageaccount2 -UseConnectedAccount
+New-AzStorageContainer -Name containertestb -Context $ctx2 -Permission Blob
+
+#Create 100 blobs
+function Create100BlobsInNewFolder 
+{
+    Write-Output "                              START CREATE BLOB"
+    #ToDo: create folder
+    New-Item -ItemType Directory -Force -Path 100_Blobs
+    for( $num = 0; $num -le 100  ; $num++ )
+    {
+        Write-Output $num
+        $fileToCreate = ".\100_Blobs\0$num.txt"
+        Set-Content $fileToCreate 'Blob'
+        Write-Output $fileToCreate
+        #Upload the New Blob into ContainerA 
+        #Set-AzStorageAccount -ResourceGroupName myResourceGroupInbal -Name inbstorageaccount1 | Set-AzStorageBlobContent -Container containertest -File .\100_Blobs\0.txt -Blob blob\0$num
+        Set-AzStorageAccount -ResourceGroupName myResourceGroupInbal -Name inbstorageaccount1 | Set-AzStorageBlobContent -Container containertest -File $fileToCreate 
+    }
+    Write-Output "finish blob"
+}
+Create100BlobsInNewFolder
+
+
+$srcStorageKey = Get-AzStorageAccountKey -Name inbstorageaccount1 -ResourceGroupName myResourceGroupInbal  
+$destStorageKey = Get-AzStorageAccountKey -Name inbstorageaccount2 -ResourceGroupName myResourceGroupInbal
+$srcContext = New-AzStorageContext -StorageAccountName inbstorageaccount1 -StorageAccountKey $srcStorageKey.Value[0]
+$destContext = New-AzStorageContext -StorageAccountName inbstorageaccount2 -StorageAccountKey $destStorageKey.Value[0]
+#$srcBlob = Get-AzStorageBlob -Container $containerName -Blob $blobName  -Context $ctx 
+#$destBlob =  $srcBlob | Copy-AzStorageBlob  -DestContainer "destcontainername" -DestBlob "destblobname"
+function copyFromAtoB { 
+
+    for( $n = 0; $n -le 100  ; $n++ )
+    {
+        Start-AzStorageBlobCopy -SrcBlob $srcBlob -SrcContainer blob -Context $srcContext -DestBlob $destBlob -DestContainer blob -DestContext $destContext
+    }
+}
+#copyFromAtoB
