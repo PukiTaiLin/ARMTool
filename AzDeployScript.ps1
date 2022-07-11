c0ae8a7b5762852f62af106a9de73a7ec493f3df
Inbal <inbal.rozenfeld@outlook.com>
2022-07-11 09:39:34 +0300

    add force

--- AzDeployScript.ps1
+++ AzDeployScript.ps1
@@ -1,3 +1,3 @@
-New-AzResourceGroup -Name myResourceGroupInbal -Location "Central US"
+New-AzResourceGroup -Name myResourceGroupInbal -Location "Central US" -force
 New-AzResourceGroupDeployment -Name createtwotorate -ResourceGroupName myResourceGroupInbal -TemplateFile '.\template1 - 1.2.json' -force
 New-AzResourceGroupDeployment -Name NewVM -ResourceGroupName myResourceGroupInbal -TemplateFile '.\VMTemplate.json' -TemplateParameterFile '.\VMTemplate.parameters.json'
